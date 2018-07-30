2c90ec4c31c33494afd1ca8c86e58fafa346bf73
Jaicke <38104697+Jaicke@users.noreply.github.com>
2018-07-30 19:16:07 -0300

    Site adicionado

---
+++ javascript/funcoes.js
@@ -0,0 +1,24 @@
+
+
+function mudaFoto (foto) {
+		document.getElementById("icone").src = foto;
+}
+
+function mudaFoto2 (foto) {
+		document.getElementById("icone2").src = foto;
+}
+function mudaFoto3 (foto) {
+		document.getElementById("icone3").src = foto;
+}
+function mudaFoto4 (foto) {
+		document.getElementById("icone4").src = foto;
+}
+function mudaFoto5 (foto) {
+		document.getElementById("icone5").src = foto;
+}
+function mudaFoto6 (foto) {
+		document.getElementById("icone6").src = foto;
+}
+
+
+
